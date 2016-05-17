042e9b75f5caa5d0b1f00a6745522ca7eb7e4932
Frank Salas <Frank.salas@gmail.com>
2016-05-17 17:32:00 -0500

    first commit. end if 2.3 Creating our base models.

---
+++ chores/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class ChoreList(models.Model):
+    name = models.CharField(max_length=100)
+    due_date = models.DateTimeField()
+
+class Chore(models.Model):
+    chore_list = models.ForeignKey(ChoreList)
+    name = models.CharField(max_length=100)
+    due_date = models.DateTimeField()
