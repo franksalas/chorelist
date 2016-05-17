3199fa815d8d0152f5ff6aebc4adddb3362c7497
Frank Salas <Frank.salas@gmail.com>
2016-05-17 17:55:33 -0500

    End of 2.6 Playing With the Data

--- chores/models.py
+++ chores/models.py
@@ -7,8 +7,15 @@ class ChoreList(models.Model):
     name = models.CharField(max_length=100)
     due_date = models.DateTimeField()
 
+    def __str__(self):
+        return self.name
+
+
 class Chore(models.Model):
     chore_list = models.ForeignKey(ChoreList)
     name = models.CharField(max_length=100)
     due_date = models.DateTimeField()
-    complete = models.BooleanField(default=True)
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
