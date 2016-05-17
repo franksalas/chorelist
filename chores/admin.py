ff0ac93f1cae5afc0cfd0edf9af263d4ecb267b0
Frank Salas <Frank.salas@gmail.com>
2016-05-17 18:29:32 -0500

    End of 3.4 Adding Related Models
    There are very few applications in the world in which you will be dealing with isolated models. Relationships are everywhere! Let's give the admin site access to some of these relationships.

--- chores/admin.py
+++ chores/admin.py
@@ -4,13 +4,20 @@ from .models import ChoreList, Chore
 
 # Register your models here.
 
+class ChoreInLine(admin.TabularInline):
+    model = Chore
+    extra = 3
+
 
 class ChoreListAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['name']}),
         ('Date Info',{'fields': ['due_date'], 'classes':['collapse']}),
+
     ]
+    inlines = [ChoreInLine]
 
 
-admin.site.register(Chore)
+
 admin.site.register(ChoreList, ChoreListAdmin)
+
