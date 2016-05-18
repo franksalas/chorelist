a72f386175439352a0747d0859cf0c4a84c6aa65
Frank Salas <Frank.salas@gmail.com>
2016-05-17 19:09:53 -0500

    End of 4.2 Adding the Rest of the Views
    Now that we understand how to create views, let's create the other views that will service the requests into this site.

--- chores/views.py
+++ chores/views.py
@@ -2,4 +2,17 @@ from django.shortcuts import render
 from  django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello from the chores app.")
+    return HttpResponse("You're at the ChoreList index")
+
+
+def detail(request, chorelist_id):
+    return HttpResponse("You're looking at Chore list #%s" % chorelist_id)
+
+
+def chores(request, chorelist_id):
+    return HttpResponse("You're looking at Chores from ChoreList #%s" % chorelist_id)
+
+
+def choredetail(request, chorelist_id, chore_id):
+    return HttpResponse("You're looking at Chore #%s  from ChoreList # %s" %(chore_id, chorelist_id))
+
